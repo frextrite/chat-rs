fba3b85361b84f0eaa3b49a8f12e7f62cb8e6cd8
Amol Grover <frextrite@gmail.com>
2023-12-26 23:57:26 +0530

    [glue] impl bi-directional handling of chat messages

--- src/client.rs
+++ src/client.rs
@@ -1,4 +1,7 @@
-use chat::{chatter_client::ChatterClient, HealthRequest};
+use std::thread;
+
+use chat::{chatter_client::ChatterClient, ChatMessage, HealthRequest};
+use tokio::sync::mpsc;
 use tonic::{transport::Endpoint, Request};
 
 pub mod chat {
@@ -12,10 +15,39 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut client = ChatterClient::new(channel);
 
     let request = Request::new(HealthRequest {});
-
     let response = client.probe_health(request).await?;
-
     println!("INFO: Received response {:?}", response);
+
+    let request = Request::new(HealthRequest {});
+    let response = client.probe_health(request).await?;
+    println!("INFO: Received response {:?}", response);
+
+    let (tx, mut rx) = mpsc::channel(4);
+    thread::spawn(move || {
+        let input = std::io::stdin();
+        let mut line = String::new();
+        loop {
+            input.read_line(&mut line).unwrap();
+            let message = ChatMessage {
+                author: "me".to_string(),
+                message: line.clone(),
+            };
+            tx.blocking_send(message).unwrap();
+            line.clear();
+        }
+    });
+    let outbound = async_stream::stream! {
+        while let Some(message) = rx.recv().await {
+            yield message;
+        }
+    };
+    let response = client
+        .send_and_receive_messages(Request::new(outbound))
+        .await?;
+    let mut inbound = response.into_inner();
+    while let Some(message) = inbound.message().await? {
+        println!("INFO: Received message {:?}", message);
+    }
 
     Ok(())
 }
